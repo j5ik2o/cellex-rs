605af15c9efcccaae7a486df6edc86eb6eed8be3
Junichi Kato <j5ik2o@gmail.com>
2025-10-27 11:36:13 +0900

    chore: remove outdated queue and stack module implementations
    Deleted deprecated `queue` and `stack` module implementations, including `mpsc`, `ring`, `stack`, and related traits, from `utils-core`. These modules are no longer in use and have been replaced by newer alternatives.

--- modules/utils-core/src/collections/queue/mpsc.rs
+++
@@ -1,9 +0,0 @@
-mod mpsc_backend;
-mod mpsc_buffer;
-mod mpsc_queue;
-/// MPSC queue trait definitions.
-pub mod traits;
-
-pub use mpsc_backend::RingBufferBackend;
-pub use mpsc_buffer::MpscBuffer;
-pub use mpsc_queue::MpscQueue;
